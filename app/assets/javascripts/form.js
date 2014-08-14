6b3d3e468d8144c183aefc2c4707bb883650bcde
davidhurley <dhurls99@gmail.com>
2014-08-14 18:09:42 -0400

    finish user sign-in page functionality

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -11,16 +11,3 @@ var app = angular.module('angularResume', ['ui.bootstrap']);
     
  }]);
 
- app.controller('UserController', ['$http', function($http) {
-    var app = this;
-
-    this.addUser = function() {
-
-      $http.post('/users.json').success(function(data){
-          console.log(data);
-         app.users.push(data);
-      });
-    };
-    
-    
- }]);
