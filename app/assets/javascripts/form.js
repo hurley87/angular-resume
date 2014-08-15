6edf00c9bbbd320174c038b518ea7bad9c89244f
davidhurley <dhurls99@gmail.com>
2014-08-14 22:04:18 -0400

    add product details and reviews to project show page

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -13,9 +13,11 @@ app.controller('AppController', ['$http', function($http) {
 
 app.controller('ProjectController', ['$http', function($http) {
     var app = this;
-    app.users = [];
-    $http.get('/users.json').success(function(data){
-        app.users = data;
-    });
-    
+    var id = $('#project-show').data('id');
+
+    app.project = [];
+    $http.get('/projects.json').success(function(data){
+        app.project = data.projects[id-1]; 
+        console.log(app.project.reviews[0].body)
+    }); 
  }]);
