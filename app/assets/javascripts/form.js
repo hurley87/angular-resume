8ab982a4fc6b4bd450e02b83d5d512b353f1316b
davidhurley <dhurls99@gmail.com>
2014-09-03 22:58:16 -0400

    add animated circles and user's ability to click on them

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -2,6 +2,7 @@
 
   var app = angular.module('angularResume', ['ui.bootstrap']);
 
+  //all users who sign up for the site 
   app.controller('UsersController', ['$http', function($http) {
     var app = this;
     app.users = [];
@@ -10,6 +11,7 @@
     });
   }]);
 
+  //all projects I've created on the site
   app.controller('ProjectsController', ['$http', function($http) {
     var app = this;
     app.projects = [];
@@ -18,11 +20,12 @@
     });
   }]);
 
+  //counter that counts up by 1 per second
   app.controller('ClockController', ['$scope', function($scope) {
-      $scope.clock = 0;
+      $scope.clock = 10;
 
       var updateClock = function() {
-        $scope.clock += 10;
+        $scope.clock += 1;
       };
 
       setInterval(function() {
@@ -30,5 +33,31 @@
       }, 1000);
 
       updateClock();
+
+
+      $scope.hits = 0;
+
+      $scope.addHit = function() {
+        $scope.hits = $scope.hits + 1;
+        return false;
+      };
+
   }]);
+
+  $(document).ready(function() {
+      // hide skill circle when user clicks them
+      $('.circle').on('click', function() {
+          $(this).hide();
+      });
+
+      //animate cirlce side to side to its tough for the user to click on them
+      setInterval(function() {
+        $('.circle').animate({
+          top: Math.random()*100,
+          left: Math.random()*300
+        }, 500);
+      }, 500);
+      
+  });
+
 
