0bf70383628ef67c5a4ead7f2cd41d43e2a877fb
davidhurley <dhurls99@gmail.com>
2014-09-08 11:33:40 -0400

    add projects to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,12 @@
 # An STI approach was used to seperate the experience between fan and brewry owner. AJAX is behind project categories and user comments. Sass is used for certain affects but mostly does in css with some Foundation help. We used Stripe for payments.', 
 # 							 link: 'http://brewgarden.herokuapp.com/' )
 
-# Project.create(name: 'From The Crowd' , 
-# 							 short: "Self-regulatring Sports Blog" , 
-# 							 description: 'For funtionality we allowed a user to save their article before posting, comment on articles and discover articles through tags. Probably the most interesting feature is the ability to vote for each article out of 100. We then ranked articles according to their average vote. If an article has an average score under 40 we destory the article after 10 votes. This is a strategy to regulate poor content.', 
-# 							 link: 'http://cryptic-hollows-5626.herokuapp.com/' )
+Project.create(name: 'From The Crowd' , 
+							 short: "Self-regulatring Sports Blog" , 
+							 description: 'For funtionality we allowed a user to save their article before posting, comment on articles and discover articles through tags. Probably the most interesting feature is the ability to vote for each article out of 100. We then ranked articles according to their average vote. If an article has an average score under 40 we destory the article after 10 votes. This is a strategy to regulate poor content.', 
+							 link: 'http://cryptic-hollows-5626.herokuapp.com/' )
 
-# Project.create(name: 'Huchre' , 
-# 							 short: "1V1 Euchre Game" , 
-# 							 description: 'I grew up playing a card game called Euchre. Never having the right amount of people to play my Mom and I invented a 1v1 version of Euchre. After many hours of testing we came up with something pretty cool. I tried to replicate the game and build a 2 player version in jQuery.', 
-# 							 link: 'http://davidbhurley.com/euchre2.html' )
+Project.create(name: 'Huchre' , 
+							 short: "1V1 Euchre Game" , 
+							 description: 'I grew up playing a card game called Euchre. Never having the right amount of people to play my Mom and I invented a 1v1 version of Euchre. After many hours of testing we came up with something pretty cool. I tried to replicate the game and build a 2 player version in jQuery.', 
+							 link: 'http://davidbhurley.com/euchre2.html' )
