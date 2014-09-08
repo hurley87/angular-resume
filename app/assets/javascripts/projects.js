3de3ad1fc90979fee5eb12a0eff31baaf2083cb6
davidhurley <dhurls99@gmail.com>
2014-09-08 11:56:14 -0400

    i think it finally works

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -110,89 +110,89 @@
   app.directive('calculator', function() {
     return {
       restrict: 'E',
-      templateUrl: './projects/calculator.html'
-        controller: function Calc($scope) {
+      templateUrl: '/projects/calculator.html',
+      controller: function Calc($scope) {
 
-          $scope.initialize = function() {
-            $scope.output = 0;
-                $scope.newNumber = true;
-                $scope.pendingOperation = null;
-                $scope.RunningTotal = null;
-                $scope.pendingValue = null;
-                $scope.lastOperation = null;
-          }
-            $scope.initialize();
-
-            $scope.updateOutput = function(value) {
-               if($scope.output == 0 || $scope.newNumber ) {
-                  $scope.output = value;
-                  $scope.newNumber = false;
-               } else {
-                  $scope.output += String(value);
-               }
-               $scope.pendingValue = $scope.output*1;
-            }
-
-            $scope.add = function () {
-                $scope.performOperation();
-                $scope.pendingOperation = '+';
-                
-            }
-
-            $scope.subtract = function () {
-              $scope.performOperation();
-              $scope.pendingOperation = '-';
-              
-            }
-
-            $scope.multiply = function() {
-              $scope.performOperation();
-              $scope.pendingOperation = '*';
-            }
-
-            $scope.calculate = function() {
-                $scope.performOperation();  
-            }
-
-            $scope.devide = function() {
-              $scope.performOperation();
-              $scope.pendingOperation = '/';
-            
-            }
-
-            $scope.clear = function() {
-               $scope.initialize();
-            }
-
-            $scope.performOperation = function() {
-
-              if($scope.pendingValue) {
-                switch($scope.pendingOperation) {
-                  case '+':
-                      $scope.runningTotal += $scope.pendingValue;
-                      break;
-                  case '-': 
-                      $scope.runningTotal -= $scope.pendingValue;
-                      break; 
-                  case '*':
-                      $scope.runningTotal *= $scope.pendingValue;
-                      break;  
-                  case '/':
-                      $scope.runningTotal /= $scope.pendingValue;
-                      break;     
-                  default:
-                      $scope.runningTotal = $scope.pendingValue;    
-                  
-                }
-              } 
-
-              $scope.output = String($scope.runningTotal);
+        $scope.initialize = function() {
+          $scope.output = 0;
               $scope.newNumber = true;
+              $scope.pendingOperation = null;
+              $scope.RunningTotal = null;
               $scope.pendingValue = null;
-         
-            }
-
+              $scope.lastOperation = null;
         }
+          $scope.initialize();
+
+          $scope.updateOutput = function(value) {
+             if($scope.output == 0 || $scope.newNumber ) {
+                $scope.output = value;
+                $scope.newNumber = false;
+             } else {
+                $scope.output += String(value);
+             }
+             $scope.pendingValue = $scope.output*1;
+          }
+
+          $scope.add = function () {
+              $scope.performOperation();
+              $scope.pendingOperation = '+';
+              
+          }
+
+          $scope.subtract = function () {
+            $scope.performOperation();
+            $scope.pendingOperation = '-';
+            
+          }
+
+          $scope.multiply = function() {
+            $scope.performOperation();
+            $scope.pendingOperation = '*';
+          }
+
+          $scope.calculate = function() {
+              $scope.performOperation();  
+          }
+
+          $scope.devide = function() {
+            $scope.performOperation();
+            $scope.pendingOperation = '/';
+          
+          }
+
+          $scope.clear = function() {
+             $scope.initialize();
+          }
+
+          $scope.performOperation = function() {
+
+            if($scope.pendingValue) {
+              switch($scope.pendingOperation) {
+                case '+':
+                    $scope.runningTotal += $scope.pendingValue;
+                    break;
+                case '-': 
+                    $scope.runningTotal -= $scope.pendingValue;
+                    break; 
+                case '*':
+                    $scope.runningTotal *= $scope.pendingValue;
+                    break;  
+                case '/':
+                    $scope.runningTotal /= $scope.pendingValue;
+                    break;     
+                default:
+                    $scope.runningTotal = $scope.pendingValue;    
+                
+              }
+            } 
+
+            $scope.output = String($scope.runningTotal);
+            $scope.newNumber = true;
+            $scope.pendingValue = null;
+       
+          }
+
+      }
     };
   });
 
